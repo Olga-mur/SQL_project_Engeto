f4171a49573badebd54c8a6a1938cfd57854db4e
Olga Murzinskaja <mazurka4u@gmail.com>
2023-12-03 10:32:32 +0100

    Add Table 2 file

---
+++ Table 2.sql
@@ -0,0 +1,14 @@
+CREATE TABLE t_olga_murzinskaja_project_SQL_secondary_final AS
+	SELECT 
+		c.country,
+		c.abbreviation,
+		c.region_in_world,
+		e.`year`,
+		e.GDP,
+		e.gini,
+		c.population AS polulation_economies, 
+		e.population AS population_countries
+	FROM countries c
+	LEFT JOIN economies e 
+	 ON c.country = e.country
+	WHERE c.continent LIKE "Europe" AND e.`year` BETWEEN 2006 AND 2018;
